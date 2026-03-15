952ace3a496fdb2ddba899b1b06e4aecc20384ed
Khôi Nguyên <khoinguyenmai17102005@gmail.com>
2026-03-15 16:58:24 +1030

    Revert "fix: ignore @chroma-core/default-embed in webpack to fix ChromaDB on Vercel"
    This reverts commit cceb0f09bd3415c6a0d71c62d89f9c8b1129d15e.

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
-  webpack: (config, { webpack }) => {
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^@chroma-core\/default-embed$/,
-      })
-    );
-    return config;
-  },
 };
 
 export default nextConfig;
